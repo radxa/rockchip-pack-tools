8147f1b15fde09ac32d2803c4aefef6c74f6d303
Tom Cubie <tom@radxa.com>
2014-02-14 16:27:16 +0800

    update bootloader version to 2.08

--- mkupdate.sh
+++ mkupdate.sh
@@ -8,5 +8,5 @@ rm -f update_tmp.img
 rm -f update.img
 
 ./afptool  -pack . update_tmp.img
-./img_maker -rk31 RK3188Loader\(L\)_V1.24.bin 1 0 0 update_tmp.img update.img
+./img_maker -rk31 RK3188Loader\(L\)_V2.08.bin 1 0 0 update_tmp.img update.img
 echo "update.img is at `pwd`/update.img"
