f12534f164ecf5c9923db798754408355bc4a5f9
Tom Cubie <tom@radxa.com>
2014-12-30 17:56:41 +0800

    run mkupdate.sh at any path

--- mkupdate.sh
+++ mkupdate.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-cd `dirname $0`
+TOP=$(cd $(dirname $0) && pwd)
+cd $TOP
 
 rm -f update_tmp.img
 rm -f update.img
 
 ./afptool  -pack . update_tmp.img
 ./img_maker -rk31 RK3188Loader\(L\)_V2.19.bin 1 0 0 update_tmp.img update.img
-echo "update.img is at `pwd`/update.img"
+echo "Image is at $TOP/update.img"
