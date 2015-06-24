a5e8df33d49a21f1d5f7a7d0e312b029a255ef7d
Tom Cubie <tom@radxa.com>
2015-06-24 17:25:04 +0800

    add rock2 series pack tools

--- mkupdate.sh
+++ mkupdate.sh
@@ -9,5 +9,5 @@ rm -f update_tmp.img
 rm -f update.img
 
 ./afptool  -pack . update_tmp.img
-./img_maker -rk31 RK3188Loader\(L\)_V2.19.bin 1 0 0 update_tmp.img update.img
+./img_maker -rk32 RK3288UbootLoader_V2.19.01.bin 1 0 0 update_tmp.img update.img
 echo "Image is at $TOP/update.img"
